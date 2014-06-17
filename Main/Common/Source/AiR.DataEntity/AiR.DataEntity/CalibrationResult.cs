4cb67d003c08ea8fe604702f9503ec722a837e77
wwbbx <wwbbx@hotmail.com>
2014-06-17 22:36:29 +0800

    delete README file.

---
+++ Main/Common/Source/AiR.DataEntity/AiR.DataEntity/CalibrationResult.cs
@@ -0,0 +1,13 @@
+﻿using AiR.DataEntity.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AiR.DataEntity
+{
+    public class CalibrationResult:ICalibrationResult
+    {
+    }
+}
